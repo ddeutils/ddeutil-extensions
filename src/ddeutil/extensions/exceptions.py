72bff1fad81a4806bbd179e1203cfe6573b47bef
Korawich Anuttra <korawich.anu@gmail.com>
2025-03-26 10:42:36 +0700

    :dart: feat: add base extension exception class.

--- src/ddeutil/extensions/exceptions.py
+++ src/ddeutil/extensions/exceptions.py
@@ -3,3 +3,6 @@
 # Licensed under the MIT License. See LICENSE in the project root for
 # license information.
 # ------------------------------------------------------------------------------
+
+
+class BaseExtensionError(Exception): ...
